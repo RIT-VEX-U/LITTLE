6c89430e841180e93947fa6cfa7cceccd5da3636
Dana Colletti <dec1701@rit.edu>
2019-02-18 21:58:59 -0500

    Skeleton of auto, will try to add real values later. Vision Sensor is a thing now

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,10 +2,10 @@
 #include "hardware.h"
 #include "drive.h"
 #define SPROCKET_ROTATION 2.3561925 // circumference in inches, distance traveled in one rotation
+#define COLOR_AIM 1 // 1 = red, 2 = blue
 
-//values need to be changed
-vision_signature_s_t RED_SIG = Vision::signature_from_utility(1, 8973, 11143, 10058, -2119, -1053, -1586, 5.4, 0);
-//vision_signature_s_t BLUE_SIG = Vision::signature_from_utility(2,...);
+pros::vision_signature RED_SIG = pros::Vision::signature_from_utility(1, -3817, -3307, -3562, 12179, 13435, 12807, 8.8, 0);//BLUE
+pros::vision_signature BLUE_SIG = pros::Vision::signature_from_utility(2, 8595, 8905, 8750, -717, -353, -535, 8.8, 0);//RED
 
 enum AutoPath
 {
@@ -20,10 +20,8 @@ AutoPath currentPosition = init;
 // set vision vision signatures
 void setSig(){
   Hardware::camera.set_signature(1, &RED_SIG);
-  //Hardware::vis.set_signature(1, &BLUE_SIG);
+  Hardware::camera.set_signature(2, &BLUE_SIG);
 }
-
-
 
 // resets chassis encoders (all?)
 void resetEncoders(){
@@ -32,8 +30,14 @@ void resetEncoders(){
 }
 
 // position robot so it's ready to shoot (vision sensor needed)
-void lineUp(){
-
+void lineUp(int sig_num){
+  while(Hardware::camera.get_by_sig(0, sig_num).left_coord > 0){ // is coordinate 0 centerd?
+    turnNoDeg(0, 100);
+  }
+  while(Hardware::camera.get_by_sig(0, sig_num).left_coord < 0){ // is coordinate 0 centerd?
+    turnNoDeg(1, 100);
+  }
+  turnNoDeg(0, 0);
 }
 
 //
@@ -44,9 +48,18 @@ void driveDist(int inch){
     drive(100, 100); //lowered speed for more accuracy
   }
   drive(-20, -20); //quick brake if needed
-  delay(500);
+  pros::delay(500);
   drive(0, 0);
   resetEncoders();
+}
+
+//Fire loaded ball
+void shoot(){
+  Hardware::leftFlywheelMotor.move(127);
+  Hardware::rightFlywheelMotor.move(127);
+  pros::delay(2000);
+  Hardware::leftFlywheelMotor.move(127);
+  Hardware::rightFlywheelMotor.move(127);
 }
 
 /**
@@ -60,56 +73,63 @@ void driveDist(int inch){
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {
-  switch(currentPosition)
+void autonomous() { // MOST IF NOT ALL OF THESE VALUES ARE GUESSES
+  switch(currentPosition) // (bc we can't know the exact angle it will turn to face the flag, human error in set up)
   {
     case init:
       setSig();
     break;
     case turnToOurFlag1:
-
+      lineUp(COLOR_AIM);
     break;
     case fireMid1:
-
+      shoot();
     break;
     case driveToPushFlag:
-
+      driveDist(48);
     break;
     case reverseFromPushFlag:
-
+      driveDist(-10);
     break;
     case turnToGetBall1:
-
+      turn(-45, 100);
     break;
     case driveToBall1:
-
+      driveDist(48);
+      turn(20, 100);
+      driveDist(10);
     break;
     case reverseFromBall1:
-
+      driveDist(-10);
     break;
     case turnToMidFlag:
-
+      turn(90, 100);
+      lineUp(COLOR_AIM);
     break;
     case fireMid2:
-
+      shoot();
     break;
     case turnToGetBall2:
-
+      turn(10, 100);
     break;
     case driveToBall2:
-
+      driveDist(30);
     break;
     case reverseFromBall2:
-
+      driveDist(-30);
     break;
     case turnToOurFlag2:
-
+      turn(-10, 100);
+      lineUp(COLOR_AIM);
     break;
     case fireHigh1:
-
+      shoot();
     break;
     case end:
-
+      // climbing alliance platform
+      driveDist(-24);
+      turn(-90, 100);
+      driveDist(48);
     break;
   }
 }
